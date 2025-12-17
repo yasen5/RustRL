818c7857113ac9aedd6dfd48c4ca03b874209d8f
yasen5 <167659798+yasen5@users.noreply.github.com>
2025-12-17 14:48:39 -0800

    Fixed some stuff

--- src/game.rs
+++ src/game.rs
@@ -1,25 +1,22 @@
-extern crate uom;
-
 use std::f32::consts::PI;
 
 use ndarray_rand::rand_distr::num_traits::ToPrimitive;
 use rand;
-use std::any::type_name_of_val;
 use uom::si::acceleration::meter_per_second_squared;
 use uom::si::angle::radian;
-use uom::si::angular_acceleration::radian_per_second_squared;
 use uom::si::angular_velocity::radian_per_second;
 use uom::si::f32::*;
 use uom::si::force::newton;
 use uom::si::length::meter;
 use uom::si::mass::kilogram;
-use uom::si::moment_of_inertia::kilogram_square_meter;
 use uom::si::time::second;
-use uom::si::torque::newton_meter;
 use uom::si::velocity::meter_per_second;
+use lazy_static::lazy_static;
 
-const ENV_BOX_WIDTH: u16 = 100;
-const ENV_BOX_HEIGHT: u16 = 100;
+lazy_static! {
+    static ref ENV_BOX_WIDTH: Length = Length::new::<meter>(100.);
+    static ref ENV_BOX_HEIGHT: Length = Length::new::<meter>(100.);
+}
 
 // TODO should this contain the engine states?
 pub struct Rocket {
@@ -45,28 +42,26 @@ impl Rocket {
 
 impl Rocket {
     pub fn new(rand_x: bool, rand_y: bool, xvel: f32, yvel: f32) -> Self {
-        let width = Length::new::<meter>((ENV_BOX_WIDTH / 20).to_f32().unwrap());
-        let height = Length::new::<meter>((ENV_BOX_HEIGHT / 20).to_f32().unwrap());
         let mass = Mass::new::<kilogram>(50.);
         Self {
             x: if rand_x {
-                Length::new::<meter>(rand::random_range(0..ENV_BOX_WIDTH).to_f32().unwrap())
+                Length::new::<meter>(rand::random_range(0..ENV_BOX_WIDTH.value.to_u32().unwrap()).to_f32().unwrap())
             } else {
-                Length::new::<meter>(ENV_BOX_WIDTH.to_f32().unwrap() / 2.)
+                Length::new::<meter>(ENV_BOX_WIDTH.value / 2.)
             },
             y: if rand_y {
-                Length::new::<meter>(rand::random_range(0..ENV_BOX_HEIGHT).into())
+                Length::new::<meter>(rand::random_range(0..ENV_BOX_HEIGHT.value.to_u32().unwrap()).to_f32().unwrap())
             } else {
-                Length::new::<meter>(ENV_BOX_HEIGHT.to_f32().unwrap() / 2.)
+                Length::new::<meter>(ENV_BOX_HEIGHT.value / 2.)
             },
             vx: Velocity::new::<meter_per_second>(xvel),
             vy: Velocity::new::<meter_per_second>(yvel),
             tilt: Angle::new::<radian>(0.),
             angular_velocity: AngularVelocity::new::<radian_per_second>(0.),
-            width: width,
-            height: height,
+            width: *ENV_BOX_WIDTH / 2.0,
+            height: *ENV_BOX_HEIGHT / 2.0,
             lander_angle: Angle::new::<radian>(PI / 3.),
-            lander_length: Length::new::<meter>((ENV_BOX_HEIGHT / 40).to_f32().unwrap()),
+            lander_length: *ENV_BOX_HEIGHT / 2.0,
             engine_strength: Force::new::<newton>(2.),
             mass: mass,
         }
@@ -82,7 +77,7 @@ impl Game {
     pub fn new() -> Self {
         Self {
             state: Rocket::new(false, false, 0., 0.),
-            steps: 5,
+            steps: 0,
         }
     }
 }
@@ -97,15 +92,17 @@ pub struct StepOutcome {
 // 2: down engine
 // 3: no engines firing
 impl Game {
+    #[allow(non_snake_case)]
     pub fn step(&mut self, choice: u8) -> () {
+        let MAX_STEPS = 100;
         let DT: Time = Time::new::<second>(0.02);
         let ENGINE_ACCEL = self.state.engine_strength / self.state.mass;
         let GRAVITY: Acceleration = Acceleration::new::<meter_per_second_squared>(9.81);
         let VERTICAL_MOI: MomentOfInertia =
             self.state.mass * self.state.width * self.state.width / 12.;
-        let HORIZONTAL_MOI: MomentOfInertia =
-            self.state.mass * self.state.height * self.state.height / 12.;
-        let SIDE_ENGINE_TORQUE: Torque = (self.state.engine_strength * self.state.height / 2.0).into();
+        let HORIZONTAL_MOI: MomentOfInertia = self.state.mass * self.state.height * self.state.height / 12.;
+        let SIDE_ENGINE_TORQUE: Torque =
+            (self.state.engine_strength * self.state.height / 2.0).into();
         let SIDE_ACCEL: AngularAcceleration = (SIDE_ENGINE_TORQUE / HORIZONTAL_MOI).into();
         match choice {
             0 => {
@@ -117,6 +114,7 @@ impl Game {
             1 => {
                 self.state.vx += ENGINE_ACCEL * DT * self.state.tilt.cos();
                 self.state.vy += ENGINE_ACCEL * DT * self.state.tilt.sin();
+                self.state.angular_velocity -= AngularVelocity::from(SIDE_ACCEL * DT);
                 Ok(())
             }
             2 => {
@@ -130,7 +128,17 @@ impl Game {
         .unwrap();
         self.state.vy -= GRAVITY * DT;
         // TODO replace with more complex ground logic
-        let mut score = 0;
+        let mut score = -1;
         let mut finished = false;
+        let x = self.state.x + self.state.vx * DT;
+        let y = self.state.x + self.state.vy * DT;
+        if (x - *ENV_BOX_WIDTH/2.0).value.abs() < (self.state.x - *ENV_BOX_WIDTH/2.0).value.abs() {
+            score += 1;
+        }
+        if self.steps > MAX_STEPS {
+            score -= 5;
+        }
+        let left_leg_pos = self.state.x + self.state``
+        let touching_ground = 
     }
 }
