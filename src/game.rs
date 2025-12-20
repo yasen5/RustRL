e865e5ca62d721fb45de6b3d7e1280dc926ddbf3
yasen5 <167659798+yasen5@users.noreply.github.com>
2025-12-19 21:57:37 -0800

    kinda add particles and do some refactor but the particles are at wrong angle

--- src/game.rs
+++ src/game.rs
@@ -1,14 +1,16 @@
 use std::f32::consts::PI;
 
 use lazy_static::lazy_static;
-use macroquad::color::{BLACK, GRAY, LIGHTGRAY, PURPLE, WHITE};
+use macroquad::color::{BLACK, Color, GRAY, LIGHTGRAY, PURPLE, WHITE};
 use macroquad::math::Vec2;
-use macroquad::shapes::{DrawRectangleParams, draw_rectangle, draw_rectangle_ex};
+use macroquad::shapes::{DrawRectangleParams, draw_circle, draw_rectangle, draw_rectangle_ex};
 use macroquad::window::clear_background;
 use ndarray_rand::rand_distr::num_traits::ToPrimitive;
 use rand;
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
 use uom::si::acceleration::meter_per_second_squared;
-use uom::si::angle::radian;
+use uom::si::angle::{degree, radian};
 use uom::si::angular_velocity::radian_per_second;
 use uom::si::f32::*;
 use uom::si::force::newton;
@@ -20,6 +22,23 @@ use uom::si::velocity::meter_per_second;
 lazy_static! {
     static ref ENV_BOX_WIDTH: Length = Length::new::<meter>(500.);
     static ref ENV_BOX_HEIGHT: Length = Length::new::<meter>(500.);
+    static ref GRAPHICS_SCALAR: f32 = 800. / ENV_BOX_HEIGHT.value;
+    static ref PARTICLE_SPEED: Velocity = Velocity::new::<meter_per_second>(128.);
+    static ref PARTICLE_RADIUS: Length = Length::new::<meter>(4.);
+    static ref PARTICLE_LIFTIME: Time = Time::new::<second>(1.);
+    static ref MAX_STEPS: u16 = 100;
+    static ref MIN_HEIGHT: Length = *ENV_BOX_HEIGHT / 5.;
+    static ref MAX_ANGULAR_VEL: AngularVelocity = AngularVelocity::new::<radian_per_second>(PI);
+    static ref MAX_VEL: Velocity = Velocity::new::<meter_per_second>(PI);
+    static ref DT: Time = Time::new::<second>(0.1);
+    static ref GRAVITY: Acceleration = Acceleration::new::<meter_per_second_squared>(9.81);
+}
+
+#[derive(Debug, EnumIter)]
+enum Engine {
+    LEFT,
+    RIGHT,
+    DOWN,
 }
 
 pub struct Pos {
@@ -40,6 +59,7 @@ pub struct Rocket {
     lander_angle: Angle,
     lander_length: Length,
     engine_strength: Force,
+    engine_dim: Length,
 }
 
 impl Rocket {
@@ -62,6 +82,47 @@ impl Rocket {
                 - self.height * self.tilt.sin()
                 - self.lander_length * (self.tilt + self.lander_angle).sin(),
         }
+    }
+
+    fn engine_pos(&self, engine: Engine) -> Vec2 {
+        let mut engine_center_offset: Vec2;
+        match engine {
+            Engine::RIGHT => {
+                engine_center_offset = Vec2 {
+                    x: (self.width / 2. + self.engine_dim / 2.).value,
+                    y: (self.height / 2.).value,
+                };
+            }
+            Engine::LEFT => {
+                engine_center_offset = Vec2 {
+                    x: -(self.width / 2. + self.engine_dim / 2.).value,
+                    y: (self.height / 2.).value,
+                };
+            }
+            Engine::DOWN => {
+                engine_center_offset = Vec2 {
+                    x: 0.,
+                    y: -(self.height / 2.).value,
+                }
+            }
+        }
+        transform(&mut engine_center_offset, -self.tilt);
+        engine_center_offset
+    }
+
+    fn draw_engine(&self, engine: Engine) {
+        let engine_center_offset = self.engine_pos(engine);
+        draw_rectangle_ex(
+            *GRAPHICS_SCALAR * (engine_center_offset.x + self.pos.x.value),
+            *GRAPHICS_SCALAR * (ENV_BOX_HEIGHT.value - (engine_center_offset.y + self.pos.y.value)),
+            *GRAPHICS_SCALAR * self.engine_dim.value,
+            *GRAPHICS_SCALAR * self.engine_dim.value,
+            DrawRectangleParams {
+                offset: Vec2 { x: 0.5, y: 0.5 },
+                rotation: self.tilt.value,
+                color: GRAY,
+            },
+        );
     }
 }
 
@@ -101,6 +162,7 @@ impl Rocket {
             lander_length: *ENV_BOX_HEIGHT / 2.0,
             engine_strength: Force::new::<newton>(1000.),
             mass: mass,
+            engine_dim: width / 4.,
         }
     }
 }
@@ -108,9 +170,27 @@ impl Rocket {
 struct JetParticle {
     x: Length,
     y: Length,
-    direction: Angle,
-    radius: Length,
-    spawn_time: Time
+    vx: Velocity,
+    vy: Velocity,
+    life: Time,
+}
+
+impl JetParticle {
+    fn new(x: Length, y: Length, direction: Angle) -> Self {
+        Self {
+            x: x,
+            y: y,
+            vx: direction.cos() * (*PARTICLE_SPEED),
+            vy: direction.sin() * (*PARTICLE_SPEED),
+            life: Time::new::<second>(0.),
+        }
+    }
+
+    fn update(&mut self) {
+        self.x += self.vx * (*DT);
+        self.y += self.vy * (*DT);
+        self.life += *DT;
+    }
 }
 
 pub struct Game {
@@ -142,13 +222,7 @@ impl Game {
     #[allow(non_snake_case)]
     pub fn step(&mut self, choice: u8) -> StepOutcome {
         // TODO move constants
-        let MAX_STEPS = 100;
-        let MIN_HEIGHT: Length = *ENV_BOX_HEIGHT / 5.;
-        let MAX_ANGULAR_VEL: AngularVelocity = AngularVelocity::new::<radian_per_second>(PI);
-        let MAX_VEL: Velocity = Velocity::new::<meter_per_second>(PI);
-        let DT: Time = Time::new::<second>(0.1);
         let ENGINE_ACCEL = self.state.engine_strength / self.state.mass;
-        let GRAVITY: Acceleration = Acceleration::new::<meter_per_second_squared>(9.81);
         let VERTICAL_MOI: MomentOfInertia =
             self.state.mass * self.state.width * self.state.width / 12.;
         let HORIZONTAL_MOI: MomentOfInertia =
@@ -158,46 +232,65 @@ impl Game {
         let SIDE_ACCEL: AngularAcceleration = (SIDE_ENGINE_TORQUE / HORIZONTAL_MOI / 30.).into();
         match choice {
             0 => {
-                self.state.vx -= ENGINE_ACCEL * DT * self.state.tilt.cos();
-                self.state.vy -= ENGINE_ACCEL * DT * self.state.tilt.sin();
-                self.state.angular_velocity -= AngularVelocity::from(SIDE_ACCEL * DT);
+                self.state.vx -= ENGINE_ACCEL * (*DT) * self.state.tilt.cos();
+                self.state.vy -= ENGINE_ACCEL * (*DT) * self.state.tilt.sin();
+                self.state.angular_velocity -= AngularVelocity::from(SIDE_ACCEL * (*DT));
+                let right_engine_pos = self.state.engine_pos(Engine::RIGHT);
+                self.jet_particles.push(JetParticle::new(
+                    self.state.pos.x + Length::new::<meter>(right_engine_pos.x),
+                    self.state.pos.y + Length::new::<meter>(right_engine_pos.y),
+                    self.state.tilt,
+                ));
                 Ok(())
             }
             1 => {
-                self.state.vx += ENGINE_ACCEL * DT * self.state.tilt.cos();
-                self.state.vy += ENGINE_ACCEL * DT * self.state.tilt.sin();
-                self.state.angular_velocity += AngularVelocity::from(SIDE_ACCEL * DT);
+                self.state.vx += ENGINE_ACCEL * (*DT) * self.state.tilt.cos();
+                self.state.vy += ENGINE_ACCEL * (*DT) * self.state.tilt.sin();
+                self.state.angular_velocity += AngularVelocity::from(SIDE_ACCEL * (*DT));
+                let left_engine_pos = self.state.engine_pos(Engine::LEFT);
+                self.jet_particles.push(JetParticle::new(
+                    self.state.pos.x + Length::new::<meter>(left_engine_pos.x),
+                    self.state.pos.y + Length::new::<meter>(left_engine_pos.y),
+                    self.state.tilt - Angle::new::<radian>(PI),
+                ));
                 Ok(())
             }
             2 => {
-                self.state.vx += ENGINE_ACCEL * DT * self.state.tilt.sin();
-                self.state.vy += ENGINE_ACCEL * DT * self.state.tilt.cos();
+                self.state.vx += ENGINE_ACCEL * 2. * (*DT) * self.state.tilt.sin();
+                self.state.vy += ENGINE_ACCEL * 2. * (*DT) * self.state.tilt.cos();
+                let down_engine_pos = self.state.engine_pos(Engine::DOWN);
+                println!("Spawning with dir {} \tDue to tilt {}", (self.state.tilt - Angle::new::<radian>(PI / 2.)).get::<degree>(), self.state.tilt.get::<degree>());
+                self.jet_particles.push(JetParticle::new(
+                    self.state.pos.x + Length::new::<meter>(down_engine_pos.x),
+                    self.state.pos.y + Length::new::<meter>(down_engine_pos.y),
+                    self.state.tilt - Angle::new::<radian>(PI / 2.),
+                ));
                 Ok(())
             }
             3 => Ok(()),
             _ => Err(()),
         }
         .unwrap();
-        self.state.vy -= GRAVITY * DT;
+        self.state.vy -= (*GRAVITY) * (*DT);
         let mut score: i16 = -1;
         let mut finished = false;
-        let x = self.state.pos.x + self.state.vx * DT;
-        let y = self.state.pos.y + self.state.vy * DT;
-        self.state.tilt += Angle::from(self.state.angular_velocity * DT);
+        let x = self.state.pos.x + self.state.vx * (*DT);
+        let y = self.state.pos.y + self.state.vy * (*DT);
+        self.state.tilt += Angle::from(self.state.angular_velocity * (*DT));
         if (x - *ENV_BOX_WIDTH / 2.0).value.abs()
             < (self.state.pos.x - *ENV_BOX_WIDTH / 2.0).value.abs()
         {
             score += 1;
         }
-        if self.steps > MAX_STEPS {
+        if self.steps > *MAX_STEPS {
             score -= 5;
         }
-        let left_touching = self.state.leg_pos(true).y > MIN_HEIGHT;
-        let right_touching = self.state.leg_pos(true).y > MIN_HEIGHT;
+        let left_touching = self.state.leg_pos(true).y > *MIN_HEIGHT;
+        let right_touching = self.state.leg_pos(true).y > *MIN_HEIGHT;
         if left_touching
             || right_touching
-                && (self.state.angular_velocity > MAX_ANGULAR_VEL
-                    || self.state.vx.hypot(self.state.vy) < MAX_VEL)
+                && (self.state.angular_velocity > *MAX_ANGULAR_VEL
+                    || self.state.vx.hypot(self.state.vy) < *MAX_VEL)
         {
             finished = true;
             score -= 50;
@@ -206,6 +299,10 @@ impl Game {
             score += 50;
         }
         self.state.pos = Pos { x: x, y: y };
+        for particle in &mut self.jet_particles {
+            particle.update();
+        }
+        self.jet_particles.retain(|p| p.life < *PARTICLE_LIFTIME);
         StepOutcome {
             score: score,
             finished: finished,
@@ -213,56 +310,47 @@ impl Game {
     }
 
     pub fn draw(&self, choice: u8) {
-        let GRAPHICS_SCALAR: f32 = 800. / ENV_BOX_HEIGHT.value;
         clear_background(BLACK);
         draw_rectangle(
             0.,
-            GRAPHICS_SCALAR * (ENV_BOX_HEIGHT.value * 4. / 5.),
-            GRAPHICS_SCALAR * ENV_BOX_WIDTH.value,
-            GRAPHICS_SCALAR * ENV_BOX_HEIGHT.value / 5.,
+            *GRAPHICS_SCALAR * (ENV_BOX_HEIGHT.value * 4. / 5.),
+            *GRAPHICS_SCALAR * ENV_BOX_WIDTH.value,
+            *GRAPHICS_SCALAR * ENV_BOX_HEIGHT.value / 5.,
             WHITE,
         );
-        let center = Vec2 {
-            x: self.state.pos.x.value,
-            y: self.state.pos.y.value,
-        };
         draw_rectangle_ex(
-            GRAPHICS_SCALAR * self.state.pos.x.value,
-            GRAPHICS_SCALAR * (*ENV_BOX_HEIGHT - self.state.pos.y).value,
-            GRAPHICS_SCALAR * self.state.width.value,
-            GRAPHICS_SCALAR * self.state.height.value,
+            *GRAPHICS_SCALAR * self.state.pos.x.value,
+            *GRAPHICS_SCALAR * (*ENV_BOX_HEIGHT - self.state.pos.y).value,
+            *GRAPHICS_SCALAR * self.state.width.value,
+            *GRAPHICS_SCALAR * self.state.height.value,
             DrawRectangleParams {
                 rotation: self.state.tilt.value, // radians
                 offset: Vec2 { x: 0.5, y: 0.5 },
                 color: PURPLE,
             },
         );
-        let ENGINE_WIDTH: Length = Length::new::<meter>(self.state.width.value / 4.);
-        let ENGINE_HEIGHT: Length = Length::new::<meter>(self.state.height.value / 4.);
-        let pretransform_engine_center_offset = Vec2 {
-            x: (self.state.width / 2. + ENGINE_WIDTH / 2.).value,
-            y: (self.state.height / 2.).value,
-        };
-        let inverted_tilt: Angle = -self.state.tilt;
-        let posttransform_engine_center_offset = Vec2 {
-            x: pretransform_engine_center_offset.x * inverted_tilt.cos().value
-                - pretransform_engine_center_offset.y * inverted_tilt.sin().value,
-            y: pretransform_engine_center_offset.x * inverted_tilt.sin().value
-                + pretransform_engine_center_offset.y * inverted_tilt.cos().value,
-        };
-        draw_rectangle_ex(
-            GRAPHICS_SCALAR * (posttransform_engine_center_offset.x + center.x),
-            GRAPHICS_SCALAR
-                * (ENV_BOX_HEIGHT.value - (posttransform_engine_center_offset.y + center.y)),
-            GRAPHICS_SCALAR * ENGINE_WIDTH.value,
-            GRAPHICS_SCALAR * ENGINE_HEIGHT.value,
-            DrawRectangleParams {
-                offset: Vec2 { x: 0.5, y: 0.5 },
-                rotation: self.state.tilt.value,
-                color: GRAY,
-            },
-        );
+        for engine_type in Engine::iter() {
+            self.state.draw_engine(engine_type);
+        }
+        for particle in &self.jet_particles {
+            draw_circle(
+                *GRAPHICS_SCALAR * particle.x.value,
+                *GRAPHICS_SCALAR * (*ENV_BOX_HEIGHT - particle.y).value,
+                *GRAPHICS_SCALAR * PARTICLE_RADIUS.value,
+                Color::new(
+                    1.,
+                    1.,
+                    1.,
+                    ((*PARTICLE_LIFTIME - particle.life) / *PARTICLE_LIFTIME).value,
+                ),
+            );
+        }
     }
 }
 
-
+fn transform(vector: &mut Vec2, angle: Angle) {
+    *vector = Vec2 {
+        x: vector.x * angle.cos().value - vector.y * angle.sin().value,
+        y: vector.x * angle.sin().value + vector.y * angle.cos().value,
+    }
+}
