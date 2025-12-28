ca14812623efcd8ea7043545b7892d408e53d52a
yasen5 <167659798+yasen5@users.noreply.github.com>
2025-12-27 18:02:10 -0700

    Refactor model and start using relu (train is still incorrect)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use lunar_lander_rl::{game, model, train};
 fn main() {
     let mut agent: model::Model = model::Model::new(4);
-    agent.add_layer(Box::new(model::LinearLayer::new(5, 256)));
-    agent.add_layer(Box::new(model::LinearLayer::new(256, 64)));
-    agent.add_layer(Box::new(model::LinearLayer::new(64, 4)));
+    agent.add_layer(5, 256);
+    agent.add_layer(256, 64);
+    agent.add_layer(64, 4);
     let mut new_game: game::Game = game::Game::new();
     train::train(&mut new_game, &mut agent);
 }
--- src/model.rs
+++ src/model.rs
@@ -1,33 +1,20 @@
-use dyn_clone::DynClone;
 use ndarray::{Array1, Array2};
 use ndarray_rand::RandomExt;
 use ndarray_rand::rand_distr::Uniform;
 
-pub trait Layer: DynClone {
-    fn forward(&mut self, prev_activation: &Array1<f32>);
-    fn backward(&mut self, prev_activation: &Array1<f32>, next_derivative: &Array1<f32>);
-    fn activation(&self) -> &Array1<f32>;
-    fn prev_derivative(&self) -> &Array1<f32>;
-}
-
-dyn_clone::clone_trait_object!(Layer);
-
-#[derive(Clone)]
 pub struct SoftmaxLayer {
     activation: Array1<f32>,
-    prev_derivative: Array1<f32>,
 }
 
 impl SoftmaxLayer {
     pub fn new(inputs: usize) -> Self {
         Self {
             activation: Array1::zeros(inputs),
-            prev_derivative: Array1::zeros(inputs),
         }
     }
 }
 
-impl Layer for SoftmaxLayer {
+impl SoftmaxLayer {
     fn forward(&mut self, prev_activation: &Array1<f32>) {
         let mut exp_sum: f32 = 0.;
         for num in prev_activation {
@@ -38,26 +25,17 @@ impl Layer for SoftmaxLayer {
         }
     }
 
-    fn backward(&mut self, prev_activation: &Array1<f32>, next_derivative: &Array1<f32>) {
-        // This assumes that the calculations are being done elsewhere, since it is
-        // really convenient to calculate derivative if you do cross-entropy on a softmax
-        self.prev_derivative = next_derivative.clone(); // TODO this is inefficient 
-    }
-
     fn activation(&self) -> &Array1<f32> {
         &self.activation
-    }
-
-    fn prev_derivative(&self) -> &Array1<f32> {
-        &self.prev_derivative
     }
 }
 
 #[derive(Clone)]
 pub struct LinearLayer {
     weights: Array2<f32>,
+    weight_gradient: Array2<f32>,
     biases: Array1<f32>,
-
+    bias_gradient: Array1<f32>,
     activation: Array1<f32>,
     prev_derivative: Array1<f32>,
 }
@@ -66,24 +44,43 @@ impl LinearLayer {
     pub fn new(inputs: usize, outputs: usize) -> Self {
         Self {
             weights: ndarray::Array2::random((outputs, inputs), Uniform::new(-1.0, 1.0).unwrap()),
+            weight_gradient: Array2::zeros((outputs, inputs)),
             biases: ndarray::Array1::random(outputs, Uniform::new(-1.0, 1.0).unwrap()),
+            bias_gradient: Array1::zeros(outputs),
             activation: ndarray::Array1::zeros(outputs),
             prev_derivative: ndarray::Array1::zeros(inputs),
         }
     }
 }
 
-impl Layer for LinearLayer {
+impl LinearLayer {
     fn forward(&mut self, prev_activation: &Array1<f32>) {
-        self.activation = self.weights.dot(prev_activation) + &self.biases;
+        self.activation =
+            (self.weights.dot(prev_activation) + &self.biases).mapv(|num: f32| f32::max(0., num));
     }
 
-    fn backward(&mut self, prev_activation: &Array1<f32>, next_derivative: &Array1<f32>) {
-        self.weights -= &next_derivative
+    fn compute_gradient(&mut self, prev_activation: &Array1<f32>, next_derivative: &Array1<f32>) {
+        self.prev_derivative = self
+            .weights
+            .dot(&(prev_activation.mapv(|x| if x > 0.0 { 1.0 } else { 0.0 }) * next_derivative));
+        self.weight_gradient -= &next_derivative
             .view()
             .insert_axis(ndarray::Axis(1))
             .dot(&prev_activation.view().insert_axis(ndarray::Axis(0)));
-        self.biases -= next_derivative;
+        self.bias_gradient -= next_derivative;
+    }
+
+    fn apply_gradient(&mut self, learning_rate: f32) {
+        self.weight_gradient *= learning_rate;
+        self.bias_gradient *= learning_rate;
+        self.weights += &self.weight_gradient;
+        self.biases += &self.bias_gradient;
+        self.zero_gradient();
+    }
+
+    fn zero_gradient(&mut self) {
+        self.weight_gradient.fill(0.);
+        self.bias_gradient.fill(0.);
     }
 
     fn activation(&self) -> &Array1<f32> {
@@ -97,7 +94,7 @@ impl Layer for LinearLayer {
 
 #[derive(Clone)]
 pub struct Model {
-    layers: Vec<Box<dyn Layer>>,
+    layers: Vec<LinearLayer>,
     num_layers: usize,
     action_space: usize,
     learning_rate: f32,
@@ -113,9 +110,15 @@ impl Model {
         }
     }
 
-    pub fn add_layer(&mut self, layer: Box<dyn Layer>) -> () {
-        self.action_space = layer.activation().len();
-        self.layers.push(layer);
+    pub fn zero_gradients(&mut self) {
+        for layer in &mut self.layers {
+            layer.zero_gradient();
+        }
+    }
+
+    pub fn add_layer(&mut self, input_size: usize, output_size: usize) -> () {
+        self.action_space = output_size;
+        self.layers.push(LinearLayer::new(input_size, output_size));
         self.num_layers += 1;
     }
 
@@ -133,19 +136,19 @@ impl Model {
     pub fn backprop(&mut self, state: Array1<f32>, loss_derivative: &mut Array1<f32>) {
         *loss_derivative *= self.learning_rate;
         let (before_layers, last_layer) = self.layers.split_at_mut(self.num_layers - 1);
-        last_layer[0].backward(
+        last_layer[0].compute_gradient(
             before_layers[before_layers.len() - 1].activation(),
             loss_derivative,
         );
         for i in (1..self.layers.len() - 2).rev() {
             let (before_layers, after_layers) = self.layers.split_at_mut(i);
             let (curr_layer, after_layers) = after_layers.split_at_mut(1);
-            curr_layer[0].backward(
+            curr_layer[0].compute_gradient(
                 before_layers[before_layers.len() - 1].activation(),
                 after_layers[0].prev_derivative(),
             );
         }
         let (first_layer, other_layers) = self.layers.split_at_mut(1);
-        first_layer[0].backward(&state, other_layers[0].prev_derivative());
+        first_layer[0].compute_gradient(&state, other_layers[0].prev_derivative());
     }
 }
